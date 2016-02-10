7fc2703c102fcc0d9efd368329b883bbaf42ae62
Christian Kellner <christian@kellner.me>
2016-02-10 15:46:09 +0100

    [fileTemplates] add Python Executable.py

---
+++ fileTemplates/Python Executable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import print_function
+from __future__ import division
+
+import sys
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Application')
+    args = parser.parse_args()
